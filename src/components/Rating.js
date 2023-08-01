5c2be69cc690ff0b762e5d99f40baf47d9a3ca45
vitordiniz25 <vitordiniz25@gmail.com>
2023-08-01 19:54:21 -0300

    rating fixed in product card

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -3,7 +3,7 @@ import { StarIcon as EmptyStartIcon } from "@heroicons/react/24/outline";
 
 export default function Rating() {
     return(
-        <div className="flex items-center ml-1">
+        <div className="flex items-center -ml-1">
             {Array.from({length:4}).map((_,index) => (
                 <StarIcon key={index} className="w-6 h-6 flex-shrink-0 text-yellow-400" />
             ))}
