ae3649f4b806fc6ea131b96761a6e69cf4088510
vitordiniz25 <vitordiniz25@gmail.com>
2023-08-03 00:36:21 -0300

    add styles and structure to product page

--- src/pages/products/[id].js
+++ src/pages/products/[id].js
@@ -1,10 +1,60 @@
 import { stripe } from "@/utils/stripe"
+import { MinusSmallIcon, PlusSmallIcon } from "@heroicons/react/24/outline"
+import { CheckIcon } from "@heroicons/react/24/solid"
+import Image from "next/image"
+import { formatCurrencyString } from "use-shopping-cart"
 
 export default function ProductPage({ product }) {
-    console.log(product)
-
     return(
-        <div>ProductPage</div>
+        <div className="container lg:max-w-screen-lg mx-auto py-12 px-6">
+            <div className="flex flex-col md:flex-row justify-between items-center space-y-8 md:space-y-0 md:space-x-12">
+                <div className="relative w-72 h-72 sm:w-96 sm:h-96">
+                    <Image 
+                        src={product.image}
+                        alt={product.name}
+                        fill
+                        style={{objectFit: "contain"}}
+                        sizes="100%"
+                        priority
+                    />
+                </div>
+
+                <div className="w-full flex-1 max-w-md border border-opacity-50 rounded-md shadow-lg p-6 bg-white">
+                    <h2 className="text-3xl font-semibold">{product.name}</h2>
+                    <p className="pt-2 flex items-center space-x-2">
+                        <CheckIcon className="text-lime-500 w-5 h-5" />
+                        <span className="font-semibold">In stock</span>
+                    </p>
+
+                    <div className="mt-4 border-t pt-4">
+                        <p className="text-gray-500">Price:</p>
+                        <p className="text-xl font-semibold">{formatCurrencyString({
+                            value: product.price,
+                            currency: product.currency,
+                        })}</p>
+                    </div>
+
+                    <div className="mt-4 border-t pt-4">
+                        <p className="text-gray-500">Quantity:</p>
+                        <div className="mt-1 flex items-center space-x-3">
+                            <button className="p-1 rounded-md hover:bg-rose-100 hover:text-rose-500">
+                                <MinusSmallIcon className="w-6 h-6 flex-shrink-0" />
+                            </button>
+                            <p className="font-semibold text-xl">0</p>
+                            <button className="p-1 rounded-md hover:bg-green-100 hover:text-green-500">
+                                <PlusSmallIcon className="w-6 h-6 flex-shrink-0" />
+                            </button>
+                        </div>
+                    </div>
+                    <button className="w-full mt-4 border border-lime-500 py-2 px-6 bg-lime-500
+                        hover:bg-lime-600 hover:border-lime-600 focus:ring-4 focus:ring-opacity-50 focus:ring-lime-500
+                        text-white disabled:opacity-50 disabled:cursor-not-allowed rounded-md
+                    ">
+                        Add to cart
+                    </button>
+                </div>
+            </div>
+        </div>
     )
 }
 
