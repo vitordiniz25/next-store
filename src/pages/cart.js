8ee22ae851bf77d16416b71dd21b5e5a151df891
vitordiniz25 <vitordiniz25@gmail.com>
2023-08-07 22:03:26 -0300

    go to checkout button

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -2,7 +2,7 @@ import Link from "next/link"
 import { useShoppingCart } from "use-shopping-cart"
 
 export default function CartPage() {
-    const { cartCount, clearCart } = useShoppingCart()
+    const { cartCount, clearCart, formattedTotalPrice } = useShoppingCart()
 
     return(
         <div className="container xl:max-w-screen-xl mx-auto py-12 px-6">
@@ -30,7 +30,24 @@ export default function CartPage() {
                 </>
             )}
 
-            
+            {cartCount > 0 && (
+                <div className="mt-12 space-y-4">
+                    <div className="flex flex-col items-end border-t py-4 mt-8">
+                        <p className="text-xl">
+                            Total:
+                            <span className="font-semibold">{formattedTotalPrice}</span>    
+                        </p>
+                        <button className="border rounded py-2 px-6 bg-yellow-500
+                            hover:bg-yellow-600 border-yellow-500 hover:border-yellow-600
+                            focus:ring-4 focus:ring-opacity-50 focus:ring-yellow-500 text-white
+                            transition-colors disabled:opacity-50 disabled:cursor-not-allowed
+                            disabled:hover:bg-yellow-500 mt-4 max-w-max
+                        ">
+                            Go to Checkout
+                        </button>
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
