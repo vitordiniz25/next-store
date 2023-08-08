f688abfe15f39ce59732b64c29091f984af00b1d
vitordiniz25 <vitordiniz25@gmail.com>
2023-08-08 19:31:18 -0300

    success, failed and loading order page

--- src/pages/api/checkout-sessions/index.js
+++ src/pages/api/checkout-sessions/index.js
@@ -1,5 +1,5 @@
 import { stripe } from "@/utils/stripe"
-import { validadeCartItems } from "use-shopping-cart/utilities"
+import { validateCartItems } from "use-shopping-cart/utilities"
 
 export default async function handler(req, res) {
     if (req.method === "POST") {
@@ -18,9 +18,18 @@ export default async function handler(req, res) {
                     image: product.images[0]
                 }
             })
-            const lineItems = validadeCartItems(products, cartDetails)
+            const lineItems = validateCartItems(products, cartDetails)
+            const session = await stripe.checkout.sessions.create({
+                mode: "payment",
+                payment_method_types: ['card'],
+                line_items: lineItems,
+                success_url: `${req.headers.origin}/success?session_id={CHECKOUT_SESSION_ID}`,
+                cancel_url: `${req.headers.origin}/cart`
+            })
+            res.status(200).json(session)
         } catch (error) {
-
+            console.log(error)
+            res.status(500).json({ statusCode: 500, message: error_message })
         }
     } else {
         res.setHeader("Allow", "POST")
